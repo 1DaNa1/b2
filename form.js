c73c390eb8821e0c12235e4ee85bed79edcb27ad
1DaNa1 <144629312+1DaNa1@users.noreply.github.com>
2024-04-04 16:22:38 +0300

    Форма_оновлення

--- form.js
+++ form.js
@@ -1,30 +1,41 @@
- // Функція для збереження даних опитування в LocalStorage
- function saveSurveyData() {
-    // Отримуємо форму
-    var form = document.getElementById("surveyForm");
-    
-    // Отримуємо дані з форми
-    var formData = new FormData(form);
-    
-    // Конвертуємо дані у об'єкт JSON
-    var jsonData = {};
-    formData.forEach(function(value, key) {
-        jsonData[key] = value;
-    });
-    
-    // Зберігаємо об'єкт JSON у LocalStorage
-    localStorage.setItem("surveyData", JSON.stringify(jsonData));
-}
-
-// Додаємо обробник події "submit" до форми
+// Додавання обробника події "submit" до форми
 document.getElementById("surveyForm").addEventListener("submit", function(event) {
-    // Забороняємо формі надсилати дані стандартним шляхом
-    event.preventDefault();
-    
-    // Викликаємо функцію для збереження даних опитування
-    saveSurveyData();
-    
-    // Очищаємо дані введені користувачем з форми
-    this.reset();
-    
+    event.preventDefault(); // Забороняємо формі надсилати дані стандартним шляхом
+    saveFormData(); // Зберігаємо дані в LocalStorage
 });
+
+// Функція для збереження даних у LocalStorage
+function saveFormData() {
+    let formData = {
+        "fullname": document.forms["surveyForm"]["fullname"].value,
+        "email": document.forms["surveyForm"]["email"].value,
+        "phone": document.forms["surveyForm"]["phone"].value,
+        "faculty": document.forms["surveyForm"]["faculty"].value,
+        "stream": document.querySelector('input[name="stream"]:checked').value,
+        "group": document.forms["surveyForm"]["group"].value,
+        "curator": [],
+        "grade": parseInt(document.forms["surveyForm"]["grade"].value),
+        "meeting_time": document.forms["surveyForm"]["meeting-time"].value,
+        "student_id": document.forms["surveyForm"]["student-id"].value
+    };
+
+    let checkboxes = document.querySelectorAll('input[name="curator"]:checked');
+    checkboxes.forEach(function(checkbox) {
+        formData.curator.push(checkbox.value);
+    });
+
+    let formEntries = JSON.parse(localStorage.getItem('formEntries'))  ;
+    formEntries.push(formData);
+    localStorage.setItem('formEntries', JSON.stringify(formEntries));
+
+    // Очищення форми
+    document.getElementById("surveyForm").reset();
+
+    // Скидання значення балів на початкове
+    document.getElementById("gradeValue").innerText = "50";
+
+    // Виведення повідомлення про успішне збереження
+    alert("Відповідь збережено");
+
+    
+};
